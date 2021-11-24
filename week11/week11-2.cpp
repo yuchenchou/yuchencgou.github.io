177ead04bd93ed50b8c8e9e0c88d6aef41d15ace
周榆真 <91170624+yuchenchou@users.noreply.github.com>
2021-11-24 21:16:34 +0800

    Add files via upload

---
+++ week11/week11-2.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int a[10000000];
+int main()
+{
+    printf("請問你想要幾個質數，最大不超過10000000，請給小一點");
+    int m;
+    scanf("%d",&m);
+    int ans=0;
+    for (int i=2;i<m;i++)
+    {
+        if(a[i]==0)
+        {
+            ans++;
+            printf("%d ",i);
+            for (int k=i+i;k<10000000;k+=i)
+            {
+                a[k] =1;
+            }
+        }
+
+    }
+}
